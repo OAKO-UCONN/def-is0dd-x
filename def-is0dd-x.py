0201b6c537e42faadf814f34503d520356592934
OAKO-UCONN <63368794+OAKO-UCONN@users.noreply.github.com>
2021-06-23 20:06:56 -0400

    Update def-is0dd-x.py

--- def-is0dd-x.py
+++ def-is0dd-x.py
@@ -1,9 +1,9 @@
 def isOdd(x):
     if (x>30 or x<0):
         return 'number is out of given range'
-    elif (x<30 or x%2==1):
+    elif x%2:
         return 'number is odd'
     else:
         return 'number is even'
 
-print(is0dd(21))
+print(is0dd(22)
